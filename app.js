00392d409a0669e12faee82ebdfc988704002d03
Sam Sander <samsander@sams-mbp.home>
2020-07-14 21:01:20 -0400

    made the deck, players and css

--- app.js
+++ app.js
@@ -1,21 +1,86 @@
-var suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
-var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-var deck = new Array();
 
+const suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+const deck = new Array();
+const palyers = new Array();
+const currentPlayer = 0;
+
+
+/*
 function createDeck()
 {
-    deck = new Array();
-    for (var i = 0 ; i < values.length; i++)
+	var deck = new Array();
+
+	for(var i = 0; i < suits.length; i++)
+	{
+		for(var x = 0; x < values.length; x++)
+		{
+			var card = {Value: values[x], Suit: suits[i]};
+			deck.push(card);
+		}
+	}
+
+	return deck;
+} 
+
+*/
+
+
+//deck 
+
+function createDeck() {
+    const deck = new Array();
+    for (let i = 0 ; i < values.length; i++)
     {
-        for(var x = 0; x < suits.length; x++)
+        for(let x = 0; x < suits.length; x++)
         {
-            var weight = parseInt(values[i]);
+            let theSymbolOne = parseInt(values[i]);
             if (values[i] == "J" || values[i] == "Q" || values[i] == "K")
-                weight = 10;
+                theSymbolOne = 10;
             if (values[i] == "A")
-                weight = 11;
-            var card = { Value: values[i], Suit: suits[x], Weight: weight };
+                theSymbolOne = 11; 
+            let card = { Value: values[i], Suit: suits[x], TheSymbolOne: theSymbolOne};
             deck.push(card);
         }
     }
+} 
+
+
+// players
+
+function createPlayers(num)
+{
+    players = new Array();
+    for(let i = 1; i <= num; i++)
+    {
+        let hand = new Array();
+        let player = { Name: 'Player ' + i, ID: i, Points: 0, Hand: hand };
+        players.push(player);
+    }
+}
+
+
+function creatPlayerPretty() {
+    document.getElementById('players').innerHTML = ''
+    for (let i = 0; i < players.length; i++) {
+
+    }
+}
+
+
+
+
+
+
+function shuffle(deck)
+{
+	for (let i = 0; i < 1000; i++)
+	{
+		const shuffle1 = Math.floor((Math.random() * deck.length));
+		const shuffle2 = Math.floor((Math.random() * deck.length));
+		//const tmp = deck[location1];
+
+		//deck[location1] = deck[location2];
+		//deck[location2] = tmp;
+	}
 }
